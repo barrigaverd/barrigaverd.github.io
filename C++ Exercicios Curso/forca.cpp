71fe0b296d6a6d5135e333ebbba53ffe110b542e
Sérgio Ugoline Junior <sergiouine@gmail.com>
2023-11-19 18:33:07 -0300

    update c++ code game

--- C++ Exercicios Curso/forca.cpp
+++ C++ Exercicios Curso/forca.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -16,8 +17,42 @@ int main(){
     
     cout << "Fale para seu amigo tampar os olhos e digite a palavra secreta:";
     cin >> palavra;
+    system("cls");
+
+    while(palavra[i] != '\0'){
+        i++;
+        tam++;
+    }
     
-    
-    
+    for (i = 0;i < 30; i++){
+        secreta[i] = '-';
+    }
+
+    while((chances > 0) && (acertos < tam)){
+        cout << "Chances restantes: " << chances << "/n/n";
+        cout << "Palavra secreta: ";
+
+        for(i = 0; i < tam; i++){
+            cout << secreta[i];
+        }
+
+        cout << "/n/nDigite uma letra: ";
+        cin >> letra[0];
+
+        for(i = 0; i < tam; i++){
+            if(palavra[i] == letra[0]){
+                acerto = true;
+                secreta[i] = palavra [i];
+                acertos++;
+            }
+        }
+        if(!acerto){
+            chances--;
+        }
+
+        acerto = false;
+        system("cls");
+    }
+        
     return 0;   
 }
