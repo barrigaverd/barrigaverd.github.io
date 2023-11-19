399152f2b868308abba162ccba831398c4535879
Sérgio Ugoline Junior <sergiouine@gmail.com>
2023-11-19 18:10:16 -0300

    Att

---
+++ C++ Exercicios Curso/forca.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    char palavra[30], letra[1], secreta[30];
+    int tam, i, chances, acertos;
+    bool acerto;
+    
+    chances = 6;
+    tam = 0;
+    i = 0;
+    acerto = false;
+    acertos = 0;
+    
+    cout << "Fale para seu amigo tampar os olhos e digite a palavra secreta:";
+    cin >> palavra;
+    
+    
+    
+    return 0;   
+}
