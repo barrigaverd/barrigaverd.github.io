8506e4c4ac823da19f0f1d3830308ae830ad326c
Sérgio Ugoline Junior <sergiouine@gmail.com>
2023-12-08 21:30:36 -0300

    Update script.js

--- IA/script.js
+++ IA/script.js
@@ -19,3 +19,67 @@ function fillDinoList() {
 
     console.log('Lista de dinossauros criada!');
 };
+
+setTimeout(() => {
+    fillDinoList()
+    controls.dispatchEvent('jump')
+}, 1000);
+
+setInterval(() => {
+    if (!game.activated) return;
+
+    const dino = dinoList[dinoIndex];
+
+    if (game.crashed) {
+        if (dino.score > bestScore) {
+            bestScore = dino.score;
+            bestRNA = dino.save();
+            console.log("Melhor pontuação:", bestScore);
+        };
+        dinoIndex++;
+    
+
+        if (dinoIndex === SAMPLES) {
+            fillDinoList();
+            dinoIndex = 0;
+            bestScore = 0;
+        }
+        game.restart();
+    };
+
+    const {tRex, horizon, currentSpeed, distanceRan, dimensions} = game;
+    dino.score = distanceRan - 2000;
+
+    const player = {
+        x: tRex.xPos,
+        y: tRex.yPos,
+        speed: currentSpeed
+    };
+
+    const [obstacle] = horizon.obstacles
+    .map((obstacle) => {
+        return {
+            x: obstacle.xPos,
+            y: obstacle.yPos
+        };
+
+    })
+    .filter((obstacle) => obstacle.x > player.x);
+
+    if (obstacle) {
+        const distance = 1 - (utils.getDistance(player, obstacle) / dimensions.WIDTH);
+        const speed = player.speed / 6;
+        const height = Math.tanh(105 - obstacle.y);
+
+        const [jump, crouch] = dino.compute([distance, speed, height]);
+
+        if (jump === crouch) return;
+        if (jump) controls.dispatchEvent('jump'); //se for verdadeira o dino pula
+        if (crouch) controls.dispatchEvent('crouch');
+    };
+}, 100);
+
+/* const s = document.createElement('script');
+s.type = 'module';
+s.src = 'http://localhost:5500/script.js'
+document.body.appendChild(s); */
